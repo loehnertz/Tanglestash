97ae1a813828175df558f2559c7bf363513798de
loehnertz <jakob2102@gmail.com>
2018-02-03 15:04:04 +0100

    Transferred the static helper methods out of the main class into a separate one

---
+++ app/helpers.js
@@ -0,0 +1,39 @@
+const Fs = require("fs");
+const Path = require("path");
+const CryptoJS = require("crypto-js");
+
+
+class TanglestashHelpers {
+    static parseFileIntoBase64(path) {
+        let buffer = new Buffer(Fs.readFileSync(Path.resolve(path)));
+        return buffer.toString('base64');
+    }
+
+    static parseStringIntoBase64(string) {
+        return new Buffer(string).toString('base64');
+    }
+
+    static parseFileFromBase64(base64) {
+        return new Buffer(base64, 'base64');
+    }
+
+    static parseStringFromBase64(base64) {
+        return new Buffer(base64, 'base64').toString('utf-8');
+    }
+
+    static encrypt(plaintext, secret) {
+        let ciphertext = CryptoJS.AES.encrypt(plaintext, secret);
+        return ciphertext.toString();
+    }
+
+    static decrypt(ciphertext, secret) {
+        let bytes = CryptoJS.AES.decrypt(ciphertext, secret);
+        try {
+            return bytes.toString(CryptoJS.enc.Utf8);
+        } catch (err) {
+            return false;
+        }
+    }
+}
+
+module.exports = TanglestashHelpers;
