fb4b77804c63b8cab29d0156f5f45163ccdc5007
loehnertz <jakob2102@gmail.com>
2018-02-03 14:37:10 +0100

    Added the method that actually performs the PoW via the ccurl library

--- ccurl-interface.js
+++ ccurl-interface.js
@@ -52,6 +52,61 @@ class CcurlInterface {
             throw err;
         });
     }
+
+    /**
+     * LOGIC:
+     * Start with last index transaction
+     * Assign it the trunk / branch which the user has supplied
+     * If there is a bundle, chain the bundle transactions via trunkTransaction together
+     *
+     * @param singleTryte The single tryte from a bundle to perform the PoW on
+     * @returns {Promise}
+     */
+    getBundleTrytes(singleTryte) {
+        return new Promise((resolve, reject) => {
+            let txObject = this.iota.utils.transactionObject(singleTryte);
+            txObject.tag = txObject.tag || txObject.obsoleteTag;
+            txObject.attachmentTimestamp = Date.now();
+            txObject.attachmentTimestampLowerBound = 0;
+            txObject.attachmentTimestampUpperBound = this.MAX_TIMESTAMP_VALUE;
+
+            // If this is the first transaction, to be processed
+            // Make sure that it's the last in the bundle and then
+            // assign it the supplied trunk and branch transactions
+            if (!this.previousTxHash) {
+                // Check if last transaction in the bundle
+                if (txObject.lastIndex !== txObject.currentIndex) {
+                    return new Error("Wrong bundle order. The bundle should be ordered in descending order from currentIndex");
+                }
+
+                txObject.trunkTransaction = this.trunkTransaction;
+                txObject.branchTransaction = this.branchTransaction;
+            } else {
+                // Chain the bundle together via the trunkTransaction (previous tx in the bundle)
+                // Assign the supplied trunkTransaciton as branchTransaction
+                txObject.trunkTransaction = this.previousTxHash;
+                txObject.branchTransaction = this.trunkTransaction;
+            }
+
+            let newTrytes = this.iota.utils.transactionTrytes(txObject);
+            this.libccurl.ccurl_pow.async(newTrytes, this.minWeightMagnitude, (err, returnedTrytes) => {
+                if (err) {
+                    reject(err);
+                } else if (!returnedTrytes) {
+                    reject(new Error("The PoW was involuntarily interrupted"));
+                }
+
+                let newTxObject = this.iota.utils.transactionObject(returnedTrytes);
+
+                // Assign the previousTxHash to this new transaction hash
+                this.previousTxHash = newTxObject.hash;
+                // Push the returned trytes to the bundle array
+                this.finalBundleTrytes.push(returnedTrytes);
+
+                resolve();
+            });
+        });
+    }
 }
 
 
